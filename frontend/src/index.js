9117042a93124733b19ee4eed6f043daccaa12c8
chayaphon bunyakan <nchayaphon.bun@gmail.com>
2021-10-24 21:21:45 +0700

    finish backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import UserContextProvider from './context/userContext';
 
 ReactDOM.render(
